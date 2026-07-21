9262bb0a9d3a1a2f1c98728ea40e68f019950859
FlameFragsC <Malebochester@proton.me>
2026-07-22 01:31:38 +0200

    feat: Update logs

--- src/lib/client/consoleWarning.ts
+++ src/lib/client/consoleWarning.ts
@@ -20,23 +20,23 @@ export function printConsoleWarning() {
 
 	console.log(
 		`%c${bigText}`,
-		'color:#ef4444; font-family: monospace; font-weight: bold; font-size: 12px;'
+		'color:#3b82f6; font-family: monospace; font-weight: bold; font-size: 12px;'
 	);
 
 	console.log(
 		'%cKeep your account safe!',
-		'color:#ef4444; font-size: 24px; font-weight: bold;'
+		"color:#3b82f6; font-family: 'Builder Sans', sans-serif; font-size: 24px; font-weight: bold;"
 	);
 
 	console.log(
 		'%cDo not send any information from here to anyone or paste any text here.\n' +
 			"If someone is asking you to copy or paste text here, then you're giving " +
 			'someone access to your account, your messages, and your servers.',
-		'color:#f3f4f6; font-size: 14px; line-height: 1.5;'
+		"color:#f3f4f6; font-family: 'Builder Sans', sans-serif; font-size: 14px; line-height: 1.5;"
 	);
 
 	console.log(
 		'%cLearn more: https://colbe.cc/privacy',
-		'color:#3b82f6; font-size: 13px; text-decoration: underline;'
+		"color:#3b82f6; font-family: 'Builder Sans', sans-serif; font-size: 13px; text-decoration: underline;"
 	);
 }
